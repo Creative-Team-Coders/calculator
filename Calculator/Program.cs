a3a8b27c75d5f2ce9b0ccb44fd28000104a85bf3
Eduardo <eduubessa@gmail.com>
2022-12-12 16:57:59 +0000

    First commit, only console GUI

---
+++ Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcao;
+
+            Console.WriteLine("\n\n------------------------------------------------------------------------------------------------------|");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                                    Creative Team Coders - Calculadora                               |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                   [A] - Adição                                    [S] - Subtração                   |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                   [M] - Multiplicação                             [D] - Divisão                     |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("|                                         [X] - Fechar aplicação                                      |");
+            Console.WriteLine("|                                                                                                     |");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------|\n");
+            Console.WriteLine("Escolhe uma opção: ");
+            
+            opcao = Console.ReadKey().KeyChar;
+            
+            Console.WriteLine(opcao);
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Documentação sobre a calculadora!
